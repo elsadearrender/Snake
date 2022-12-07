3ae1081ce8859501fd3cc35156d8ef932f3ab740
elsadearrender <steny699@gmail.com>
2022-12-07 17:35:01 -0400

    Add snake eats guanabanas logic

--- constants.py
+++ constants.py
@@ -11,3 +11,11 @@ BG_COLOR = (25, 203, 54)
 
 # Size snake body
 SIZE_SNAKE = 32
+
+# Appple size
+APPLE_SIZE = 32
+
+# Score
+SCORE = 0
+SCORE_x = 10
+SCORE_y = 10
--- main.py
+++ main.py
@@ -3,6 +3,25 @@ import pygame
 from pygame.locals import *
 import constants
 import time
+import random
+
+
+class Guanabana():
+    def __init__(self, parent_window):
+        self.image = pygame.image.load("images\guanabana.png")
+        self.parent_window = parent_window
+        self.x = 120
+        self.y = 120
+
+    def draw(self):
+        self.parent_window.blit(self.image, (self.x, self.y))
+        pygame.display.flip()
+
+    # Move method
+    def move(self):
+        self.x = random.randint(32, 768)
+        self.y = random.randint(32, 568)
+
 
 class Snake():
     def __init__(self, parent_window, length):
@@ -13,6 +32,12 @@ class Snake():
         self.length = length
         self.x = [32]*length
         self.y = [32]*length
+
+    def increse_length(self):
+        self.length += 1
+        self.x.append(-1)
+        self.y.append(-1)
+
 
     def draw(self):
         self.parent_window.fill(constants.BG_COLOR)
@@ -68,8 +93,27 @@ class Game():
         self.snake = Snake(self.window, 2)
         self.snake.draw()
 
+        self.guanabana = Guanabana(self.window)
+        self.guanabana.draw()
         
         pygame.display.update()
+
+    # Collition logic
+    def is_collition(self, x1, y1, x2, y2):
+        if x1 >= x2 and x1 < x2 + constants.APPLE_SIZE:
+            if y1 >= y2 and y1 < y2 + constants.APPLE_SIZE:
+                return True
+        return False
+
+    # Play mathod
+    def play(self):
+        self.snake.walk()
+        self.guanabana.draw() 
+
+        if self.is_collition(self.snake.x[0], self.snake.y[0], self.guanabana.x, self.guanabana.y):
+            #print("JH")
+            self.snake.increse_length()
+            self.guanabana.move()
 
     def run(self):
         running = True
@@ -96,8 +140,12 @@ class Game():
                 elif event.type == QUIT:
                     running = False
 
-            self.snake.walk()
+            self.play()
             time.sleep(0.2)
 
 game = Game()
 game.run()
+
+
+
+
